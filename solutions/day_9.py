0ebcc52cef95a3d498ee88e4742139a71de44af2
Paulina Kozioł <pk.koziol49@gmail.com>
2022-12-12 19:46:06 +0100

    day 9 - final

--- solutions/day_9.py
+++ solutions/day_9.py
@@ -17,65 +17,38 @@ def does_tail_touch_head(tail_coo, head_coo):
     return x_distance <= 1 and y_distance <= 1
 
 
-def compute_tail_position_move_right(T_x, T_y, H_x, H_y):
+def compute_tail_position(T_x, T_y, H_x, H_y):
     new_T_position = []
-    if T_y == H_y:
-        # -1 or 1
-        new_T_position.append(T_x + 1)
-        new_T_position.append(T_y)
-    else:
-        # diagonal move
-        new_T_position.append(T_x + 1)
+    if T_x == H_x:
+        new_T_position.append(T_x)
         if T_y < H_y:
             new_T_position.append(T_y + 1)
         else:
             new_T_position.append(T_y - 1)
-    return new_T_position
-
-
-def compute_tail_position_move_left(T_x, T_y, H_x, H_y):
-    new_T_position = []
-    new_T_position.append(T_x - 1)
-    if T_y == H_y:
-        # -1 or 1
-        new_T_position.append(T_y)
     else:
-        # diagonal move
-        if T_y < H_y:
-            new_T_position.append(T_y + 1)
-        else:
-            new_T_position.append(T_y - 1)
-    return new_T_position
-
-
-def compute_tail_position_move_up(T_x, T_y, H_x, H_y):
-    new_T_position = []
-    if T_x == H_x:
-        # -1 or 1
-        new_T_position.append(T_x)
-    else:
-        # diagonal move
         if T_x < H_x:
             new_T_position.append(T_x + 1)
         else:
             new_T_position.append(T_x - 1)
-    new_T_position.append(T_y + 1)
-    return new_T_position
-
-
-def compute_tail_position_move_down(T_x, T_y, H_x, H_y):
-    new_T_position = []
-    if T_x == H_x:
-        # -1 or 1
-        new_T_position.append(T_x)
-    else:
-        # diagonal move
-        if T_x < H_x:
-            new_T_position.append(T_x + 1)
+        if T_y == H_y:
+            new_T_position.append(T_y)
         else:
-            new_T_position.append(T_x - 1)
-    new_T_position.append(T_y - 1)
+            if T_y < H_y:
+                new_T_position.append(T_y + 1)
+            else:
+                new_T_position.append(T_y - 1)
     return new_T_position
+
+
+def modify_rope(knots_positions, T_positions):
+    for i_of_knot in range(0, len(knots_positions) - 1):
+        knot_current_head = knots_positions[i_of_knot]
+        current_tail = knots_positions[i_of_knot + 1]
+        if not does_tail_touch_head(current_tail, knot_current_head):
+            knots_positions[i_of_knot + 1] = \
+                compute_tail_position(current_tail[0], current_tail[1], knot_current_head[0], knot_current_head[1])
+    real_tail = knots_positions[-1]
+    T_positions.add((real_tail[0], real_tail[1]))
 
 
 # initial position
@@ -91,29 +64,28 @@ for move in moves:
             H_position[0] = H_position[0] + 1
             if not does_tail_touch_head(T_position, H_position):
                 # compute tail position
-                T_position = compute_tail_position_move_right(T_position[0],T_position[1], H_position[0], H_position[1])
+                T_position = compute_tail_position(T_position[0],T_position[1], H_position[0], H_position[1])
                 T_positions.add((T_position[0], T_position[1]))
     if direction == 'L':
         for i in range(0, steps_n):
             H_position[0] = H_position[0] - 1
             if not does_tail_touch_head(T_position, H_position):
-                T_position = compute_tail_position_move_left(T_position[0],T_position[1], H_position[0], H_position[1])
+                T_position = compute_tail_position(T_position[0],T_position[1], H_position[0], H_position[1])
                 T_positions.add((T_position[0], T_position[1]))
     if direction == 'U':
         for i in range(0, steps_n):
             H_position[1] = H_position[1] + 1
             if not does_tail_touch_head(T_position, H_position):
-                T_position = compute_tail_position_move_up(T_position[0],T_position[1], H_position[0], H_position[1])
+                T_position = compute_tail_position(T_position[0],T_position[1], H_position[0], H_position[1])
                 T_positions.add((T_position[0], T_position[1]))
     if direction == 'D':
         for i in range(0, steps_n):
             H_position[1] = H_position[1] - 1
             if not does_tail_touch_head(T_position, H_position):
-                T_position = compute_tail_position_move_down(T_position[0],T_position[1], H_position[0], H_position[1])
+                T_position = compute_tail_position(T_position[0],T_position[1], H_position[0], H_position[1])
                 T_positions.add((T_position[0], T_position[1]))
 
-# print(len(T_positions))
-
+print(len(T_positions))
 # part 2
 knots_positions = [[0, 0] for i in range(0, 10)]
 T_positions = {(0, 0)}
@@ -125,50 +97,21 @@ for move in moves:
         # move first head
         for i in range(0, steps_n):
             knots_positions[0][0] = knots_positions[0][0] + 1
-            for i_of_knot, knot_current_head in enumerate(knots_positions[:-1]):
-                current_tail = knots_positions[i_of_knot + 1]
-                if not does_tail_touch_head(current_tail, knot_current_head):
-                    knots_positions[i_of_knot + 1] = \
-                        compute_tail_position_move_right(current_tail[0],current_tail[1], knot_current_head[0], knot_current_head[1])
-            real_tail = knots_positions[-1]
-            T_positions.add((real_tail[0], real_tail[1]))
+            modify_rope(knots_positions, T_positions)
     if direction == 'L':
         # move first head
         for i in range(0, steps_n):
             knots_positions[0][0] = knots_positions[0][0] - 1
-            for i_of_knot, knot_current_head in enumerate(knots_positions[:-1]):
-                current_tail = knots_positions[i_of_knot + 1]
-                if not does_tail_touch_head(current_tail, knot_current_head):
-                    knots_positions[i_of_knot + 1] = \
-                        compute_tail_position_move_left(current_tail[0],current_tail[1], knot_current_head[0], knot_current_head[1])
-            real_tail = knots_positions[-1]
-            T_positions.add((real_tail[0], real_tail[1]))
+            modify_rope(knots_positions, T_positions)
     if direction == 'U':
         # move first head
         for i in range(0, steps_n):
             knots_positions[0][1] = knots_positions[0][1] + 1
-            for i_of_knot, knot_current_head in enumerate(knots_positions[:-1]):
-                current_tail = knots_positions[i_of_knot + 1]
-                if not does_tail_touch_head(current_tail, knot_current_head):
-                    knots_positions[i_of_knot + 1] = \
-                        compute_tail_position_move_up(current_tail[0],current_tail[1], knot_current_head[0], knot_current_head[1])
-            real_tail = knots_positions[-1]
-            T_positions.add((real_tail[0], real_tail[1]))
+            modify_rope(knots_positions, T_positions)
     if direction == 'D':
         # move first head
         for i in range(0, steps_n):
             knots_positions[0][1] = knots_positions[0][1] - 1
-            for i_of_knot, knot_current_head in enumerate(knots_positions[:-1]):
-                current_tail = knots_positions[i_of_knot + 1]
-                if not does_tail_touch_head(current_tail, knot_current_head):
-                    knots_positions[i_of_knot + 1] = \
-                        compute_tail_position_move_down(current_tail[0],current_tail[1], knot_current_head[0], knot_current_head[1])
-            real_tail = knots_positions[-1]
-            T_positions.add((real_tail[0], real_tail[1]))
-    print(knots_positions)
-    print("--------")
+            modify_rope(knots_positions, T_positions)
 
 print(len(T_positions))
-
-
-
