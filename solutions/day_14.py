87162fe40c482860984646d2e5ae1864989e89bf
Paulina Kozioł <pk.koziol49@gmail.com>
2022-12-23 21:27:22 +0100

    day 14

---
+++ solutions/day_14.py
@@ -0,0 +1,102 @@
+from read import read_input
+
+data = read_input('day_14.txt')
+
+
+def simulate_sand():
+    sand_position = (500, 0)
+    while True:
+        # sand falls
+        while True:
+            down = (sand_position[0], sand_position[1] + 1)
+            left_down = (sand_position[0] - 1, sand_position[1] + 1)
+            right_down = (sand_position[0] + 1, sand_position[1] + 1)
+            possible_new_position = list(
+                filter(lambda sand_pos: sand_pos not in paths_points, [down, left_down, right_down]))
+            if len(possible_new_position) == 0:
+                break
+            else:
+                sand_position = possible_new_position.pop(0)
+            # infinite falling
+            if len(list(filter(lambda p: p[1] > sand_position[1], paths_points))) == 0:
+                return
+        paths_points.add(sand_position)
+        sand_position = (500, 0)  # new sand grain
+
+
+def simulate_sand_with_floor(floor_y):
+    sand_position = (500, 0)
+    while True:
+        # sand falls
+        while True:
+            down = (sand_position[0], sand_position[1] + 1)
+            left_down = (sand_position[0] - 1, sand_position[1] + 1)
+            right_down = (sand_position[0] + 1, sand_position[1] + 1)
+            possible_new_position = list(
+                filter(lambda sand_pos: sand_pos not in paths_points and sand_pos[1] < floor_y, [down, left_down, right_down]))
+            if len(possible_new_position) == 0:
+                if sand_position == (500, 0):
+                    paths_points.add(sand_position)
+                    return
+                break
+            else:
+                sand_position = possible_new_position.pop(0)
+        paths_points.add(sand_position)
+        sand_position = (500, 0)  # new sand grain
+
+
+# part 1
+# initialize 'matrix' of paths; list of stone's coordinates
+paths_points = set()
+for path in data:
+    points = path.split(' -> ')
+    for i, point in enumerate(points):
+        # check if no last point, then end of path
+        if i + 1 >= len(points):
+            break
+        first = point.split(',')
+        first_point = (int(first[0]), int(first[1]))
+        second = points[i + 1].split(',')
+        second_point = (int(second[0]), int(second[1]))
+        step_for_X = 1 if first_point[0] <= second_point[0] else -1
+        step_for_Y = 1 if first_point[1] <= second_point[1] else -1
+        for x in range(first_point[0], second_point[0] + step_for_X, step_for_X):
+            for y in range(first_point[1], second_point[1] + step_for_Y, step_for_Y):
+                paths_points.add((x, y))
+
+# start pouring sand
+number_of_path_points = len(paths_points)
+# part 1
+# simulate_sand()
+floor_high = max([p[1] for p in paths_points]) + 2
+# part 2
+simulate_sand_with_floor(floor_high)
+
+# how many new grains
+print(len(paths_points) - number_of_path_points)
+
+# y
+# print(paths_points)
+#
+# ss = {(494, 9),
+#       (495, 9),
+#       (496, 9),
+#       (497, 9),
+#       (498, 9),
+#       (499, 9),
+#       (500, 9),
+#       (501, 9),
+#       (502, 9),
+#       (502, 8),
+#       (502, 7),
+#       (502, 6),
+#       (502, 5),
+#       (502, 4),
+#       (503, 4),
+#       (496, 6),
+#       (497, 6),
+#       (498, 6),
+#       (498, 5),
+#       (498, 4)}
+#
+# print(ss == paths_points)
